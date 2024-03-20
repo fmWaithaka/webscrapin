9fd3c44d1bb17aa13c3c2e89028a47bda44cc885
WaithakaFM <franco313mwas@gmail.com>
2024-03-20 16:34:41 +0300

    second commit

--- index.ts
+++ index.ts
@@ -1,18 +1,29 @@
 import puppeteer, { Browser } from "puppeteer";
 
 const url =
-  "https://www.google.com/travel/flights/search?tfs=CBwQAhpEEgoyMDI0LTA0LTAyIh8KA0pGSxIKMjAyNC0wNC0wMhoDTEdXKgJaMDIDNzAyagcIARIDSkZLcgwIAxIIL20vMDRqcGwaIxIKMjAyNC0wNC0wNGoMCAMSCC9tLzA0anBscgcIARIDSkZLQAFIAXABggELCP___________wGYAQE&tfu=EgIIASIDEgEw";
+  "https://www.skyscanner.net/transport/flights/laxa/lond/240401/240403/?adultsv2=1&cabinclass=economy&childrenv2=&inboundaltsenabled=false&outboundaltsenabled=false&preferdirects=false&ref=home&rtn=1";
 const main = async () => {
   const browser: Browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
-  await page.goto(url);
+  await page.goto(url, { waitUntil: "networkidle0" });
 
-  const data = await page.evaluate(() => {
-    const priceElement = document.querySelector(
-      'span[aria-label="71351 Kenyan shillings"]'
+  const data = await page.evaluate((url) => {
+    const priceData = Array.from(
+      document.querySelectorAll(".Price_mainPriceContainer__MDM3O")
     );
-    return priceElement ? (priceElement as HTMLElement).innerText.trim() : null;
-  });
+    const prices = priceData.map((price: any) => {
+      console.log(price); // Log the price object
+      const priceElement = price.querySelector(
+        ".BpkText_bpk-text__MWZkY.BpkText_bpk-text--lg__NjNhN"
+      );
+      console.log(priceElement); // Log the selected element
+      return {
+        price: priceElement?.textContent,
+      };
+    });
+
+    return prices;
+  }, url);
 
   console.log(data);
 
